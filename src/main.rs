d80d4656698914c8f126a6feb646bfad18c97126
Misty De Meo <mistydemeo@gmail.com>
2017-11-19 17:21:24 +1100

    Add constants for colour values

--- src/main.rs
+++ src/main.rs
@@ -19,6 +19,13 @@ extern crate png;
 extern crate regex;
 use regex::Regex;
 
+// Each pixel is two bits; there are four pixels in each byte.
+// This provides mappings between the colour value and its definition in bits.
+const BITS_TRANSPARENT : [u8; 2] = [0, 0];
+const BITS_GREY        : [u8; 2] = [0, 1];
+const BITS_DARK_BLUE   : [u8; 2] = [1, 1];
+const BITS_LIGHT_BLUE  : [u8; 2] = [1, 0];
+
 fn list_tiles(input_dir : &Path) -> Result<Paths, String> {
     if !input_dir.exists() {
         return Err(format!("Directory does not exist: {}", input_dir.to_string_lossy()));
