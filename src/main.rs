291a4adb7fceeb15e86641817f4a73b8b290ad53
Misty De Meo <mistydemeo@gmail.com>
2017-11-19 18:33:28 +1100

    Correct the colour mapping

--- src/main.rs
+++ src/main.rs
@@ -18,9 +18,9 @@ use regex::Regex;
 
 // Each pixel is two bits; there are four pixels in each byte.
 // This provides mappings between the colour value and its definition in bits.
-const BITS_TRANSPARENT : [u8; 2] = [0, 0];
+const BITS_TRANSPARENT : [u8; 2] = [1, 1];
 const BITS_GREY        : [u8; 2] = [0, 1];
-const BITS_DARK_BLUE   : [u8; 2] = [1, 1];
+const BITS_DARK_BLUE   : [u8; 2] = [0, 0];
 const BITS_LIGHT_BLUE  : [u8; 2] = [1, 0];
 
 fn list_tiles(input_dir : &Path) -> Result<Paths, String> {
