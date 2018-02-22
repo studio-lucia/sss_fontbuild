74c5dc577f2f3db9f62eed2a7c9ad6e141717087
Misty De Meo <mistydemeo@gmail.com>
2018-02-21 20:37:41 -0800

    Add support for inserting data into SYSTEM.DAT

--- src/consts.rs
+++ src/consts.rs
@@ -4,3 +4,18 @@ pub const BITS_TRANSPARENT : [u8; 2] = [1, 1];
 pub const BITS_GREY        : [u8; 2] = [0, 1];
 pub const BITS_DARK_BLUE   : [u8; 2] = [0, 0];
 pub const BITS_LIGHT_BLUE  : [u8; 2] = [1, 0];
+
+// Some things need to differentiate between the two games' SYSTEM.DAT files
+pub enum Game {
+    SSS,
+    SSSC,
+}
+
+pub const SSS_SYSTEM_DAT_SIZE        : u64 = 252232;
+pub const SSSC_SYSTEM_DAT_SIZE       : u64 = 258552;
+pub const SSS_FONT_START             : u64 = 0x16FC4;
+pub const SSS_FONT_LEN_UNCOMPRESSED  : u64 = 29376;
+pub const SSS_FONT_LEN_COMPRESSED    : u64 = 28097;
+pub const SSSC_FONT_START            : u64 = 0x1757C;
+pub const SSSC_FONT_LEN_UNCOMPRESSED : u64 = 32256;
+pub const SSSC_FONT_LEN_COMPRESSED   : u64 = 30885;
--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,7 @@ use quicli::prelude::*;
 use std::result::Result;
 
 extern crate sss_fontbuild;
+use sss_fontbuild::consts::*;
 use sss_fontbuild::utils;
 
 #[derive(StructOpt, Debug)]
@@ -18,6 +19,9 @@ struct Opt {
     input: PathBuf,
     #[structopt(help = "Font file to write to", parse(from_os_str))]
     target: PathBuf,
+    #[structopt(short = "i", long = "insert",
+                help = "Insert font into the game's SYSTEM.DAT")]
+    insert: bool,
     #[structopt(short = "a", long = "append",
                 help = "Append extra data to the end of the file",
                 parse(from_os_str))]
@@ -27,28 +31,13 @@ struct Opt {
     compress: bool,
 }
 
-main!(|args: Opt| {
-    let mut target_file;
-    match File::create(&args.target) {
-        Ok(f) => target_file = f,
-        Err(e) => {
-            println!("Unable to open target file {}!\n{}", args.target.to_string_lossy(), e);
-            exit(1);
-        }
-    }
+fn main_create(args: Opt) -> quicli::prelude::Result<()> {
+    let mut target_file = File::create(&args.target)?;
 
     let mut append_data : Vec<u8>;
     match args.append {
         Some(append) => {
-            let append_file;
-            match File::open(&append) {
-                Ok(f) => append_file = f,
-                Err(e) => {
-                    println!("Unable to open append file {}!\n{}", append.to_string_lossy(), e);
-                    exit(1);
-                }
-            }
-
+            let append_file = File::open(&append)?;
             let mut buf_reader = BufReader::new(append_file);
             append_data = vec![];
             buf_reader.read_to_end(&mut append_data)?;
@@ -78,4 +67,54 @@ main!(|args: Opt| {
         utils::write_uncompressed(imagedata, &target_file)?;
     }
     target_file.write_all(&append_data)?;
+
+    return Ok(());
+}
+
+fn main_append(args: Opt) -> quicli::prelude::Result<()> {
+    let game;
+    let target_size = args.target.metadata()?.len();
+    match target_size {
+        SSS_SYSTEM_DAT_SIZE  => game = Game::SSS,
+        SSSC_SYSTEM_DAT_SIZE => game = Game::SSSC,
+        _ => {
+            println!("Couldn't recognize provided SYSTEM.DAT file!");
+            println!("Provided size, {}, doesn't match known files.", target_size);
+            exit(1);
+        }
+    }
+
+    let mut codepoints : Vec<u8> = vec![];
+    let mut imagedata : Vec<u8> = vec![];
+
+    for file in utils::list_tiles(&args.input)?.filter_map(Result::ok) {
+        let codepoint = utils::parse_codepoint_from_filename(&file.to_string_lossy())?;
+        codepoints.push(codepoint);
+
+        match utils::decode_png(&file) {
+            Ok(bytes) => imagedata.extend(bytes),
+            Err(e) => {
+                println!("Unable to parse image data for file {}!\n{}", &file.to_string_lossy(), e);
+                exit(1);
+            }
+        }
+    }
+
+    let mut system_dat_data = vec![];
+    File::open(&args.target)?.read_to_end(&mut system_dat_data)?;
+
+    let altered_data = utils::insert_data_into_file(imagedata, system_dat_data, game)?;
+
+    let mut target_file = File::create(&args.target)?;
+    target_file.write_all(&altered_data)?;
+
+    return Ok(());
+}
+
+main!(|args: Opt| {
+    if args.insert {
+        main_append(args)?;
+    } else {
+        main_create(args)?;
+    }
 });
